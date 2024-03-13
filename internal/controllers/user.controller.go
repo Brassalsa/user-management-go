8d2324192702cdad6cf4721df39e364cb0dd7620
Brassalsa <sauravchauhan2k@gmail.com>
2024-03-13 16:50:56 +0530

    refactor user controller

--- internal/controllers/user.controller.go
+++ internal/controllers/user.controller.go
@@ -1,16 +1,30 @@
 package controllers
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/Brassalsa/user-management-go/pkg/helpers"
 )
 
-func LoginUser(w http.ResponseWriter, _ *http.Request) {
+type LoginUsers struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+func HandleLoginUser(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	params := LoginUsers{}
+	err := decoder.Decode(&params)
+	if err !=nil {
+		helpers.RespondWithError(w, 400, fmt.Sprint("Error parsing json: ", err))
+		return
+	}
+	fmt.Print(params)
 
-	helpers.RespondWithJSON(w, 200, "User created")
+	helpers.RespondWithJSON(w, 200, "Logged in")
 }
 
-func RegisterUser(w http.ResponseWriter, _ *http.Request) {
-	helpers.RespondWithJSON(w, 200, "User created")
+func HandleRegisterUser(w http.ResponseWriter, _ *http.Request) {
+	helpers.RespondWithJSON(w, 201, "User created successfully!")
 }
